e834e0bca47a75cdf9a8e19ef74c197eeb6ac957
Eric Davenport <55724749+EricDavenport@users.noreply.github.com>
2025-03-18 14:32:40 -0400

    Initial Commit

---
+++ Marvel_Comicbook_Lore/Marvel_Comicbook_LoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_Comicbook_LoreApp.swift
+//  Marvel_Comicbook_Lore
+//
+//  Created by Eric Davenport on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Marvel_Comicbook_LoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Marvel_Comicbook_LoreTests/Marvel_Comicbook_LoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_Comicbook_LoreTests.swift
+//  Marvel_Comicbook_LoreTests
+//
+//  Created by Eric Davenport on 3/18/25.
+//
+
+import Testing
+@testable import Marvel_Comicbook_Lore
+
+struct Marvel_Comicbook_LoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
